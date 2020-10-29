717cbf986650c0d2375b0f833602925ca355bfc2
Ketrin <ketrin632@gmail.com>
2020-10-30 00:59:38 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace _2__Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainDishGroupBox = new System.Windows.Forms.GroupBox();
+            this.carpRadioButton = new System.Windows.Forms.RadioButton();
+            this.meatRadioButton = new System.Windows.Forms.RadioButton();
+            this.potatoesRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.pilafRadioButton = new System.Windows.Forms.RadioButton();
+            this.borchRadioButton = new System.Windows.Forms.RadioButton();
+            this.drinksGroupBox = new System.Windows.Forms.GroupBox();
+            this.waterRadioButton = new System.Windows.Forms.RadioButton();
+            this.latteRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.coffeeRadioButton = new System.Windows.Forms.RadioButton();
+            this.teaRadioButton = new System.Windows.Forms.RadioButton();
+            this.juiceRadioButton = new System.Windows.Forms.RadioButton();
+            this.dessertsGroupBox = new System.Windows.Forms.GroupBox();
+            this.iceCreamGadioButton6 = new System.Windows.Forms.RadioButton();
+            this.chocoRadioButton = new System.Windows.Forms.RadioButton();
+            this.fruitsRadioButton = new System.Windows.Forms.RadioButton();
+            this.cakeRadioButton = new System.Windows.Forms.RadioButton();
+            this.puddingGadioButton10 = new System.Windows.Forms.RadioButton();
+            this.orderButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.mainDishGroupBox.SuspendLayout();
+            this.drinksGroupBox.SuspendLayout();
+            this.dessertsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainDishGroupBox
+            // 
+            this.mainDishGroupBox.Controls.Add(this.carpRadioButton);
+            this.mainDishGroupBox.Controls.Add(this.meatRadioButton);
+            this.mainDishGroupBox.Controls.Add(this.potatoesRadioButton2);
+            this.mainDishGroupBox.Controls.Add(this.pilafRadioButton);
+            this.mainDishGroupBox.Controls.Add(this.borchRadioButton);
+            this.mainDishGroupBox.Location = new System.Drawing.Point(21, 22);
+            this.mainDishGroupBox.Name = "mainDishGroupBox";
+            this.mainDishGroupBox.Size = new System.Drawing.Size(186, 146);
+            this.mainDishGroupBox.TabIndex = 0;
+            this.mainDishGroupBox.TabStop = false;
+            this.mainDishGroupBox.Text = "Main Dish";
+            // 
+            // carpRadioButton
+            // 
+            this.carpRadioButton.AutoSize = true;
+            this.carpRadioButton.Location = new System.Drawing.Point(6, 111);
+            this.carpRadioButton.Name = "carpRadioButton";
+            this.carpRadioButton.Size = new System.Drawing.Size(107, 17);
+            this.carpRadioButton.TabIndex = 4;
+            this.carpRadioButton.Tag = "65";
+            this.carpRadioButton.Text = "Baked carp - 65₴";
+            this.carpRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // meatRadioButton
+            // 
+            this.meatRadioButton.AutoSize = true;
+            this.meatRadioButton.Location = new System.Drawing.Point(6, 88);
+            this.meatRadioButton.Name = "meatRadioButton";
+            this.meatRadioButton.Size = new System.Drawing.Size(111, 17);
+            this.meatRadioButton.TabIndex = 3;
+            this.meatRadioButton.Tag = "100";
+            this.meatRadioButton.Text = "Meat steak - 100₴";
+            this.meatRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // potatoesRadioButton2
+            // 
+            this.potatoesRadioButton2.AutoSize = true;
+            this.potatoesRadioButton2.Location = new System.Drawing.Point(6, 65);
+            this.potatoesRadioButton2.Name = "potatoesRadioButton2";
+            this.potatoesRadioButton2.Size = new System.Drawing.Size(137, 17);
+            this.potatoesRadioButton2.TabIndex = 2;
+            this.potatoesRadioButton2.Tag = "15";
+            this.potatoesRadioButton2.Text = "Mashed potatoes - 15 ₴";
+            this.potatoesRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // pilafRadioButton
+            // 
+            this.pilafRadioButton.AutoSize = true;
+            this.pilafRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.pilafRadioButton.Name = "pilafRadioButton";
+            this.pilafRadioButton.Size = new System.Drawing.Size(72, 17);
+            this.pilafRadioButton.TabIndex = 1;
+            this.pilafRadioButton.Tag = "40";
+            this.pilafRadioButton.Text = "Pilaf - 40₴";
+            this.pilafRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // borchRadioButton
+            // 
+            this.borchRadioButton.AutoSize = true;
+            this.borchRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.borchRadioButton.Name = "borchRadioButton";
+            this.borchRadioButton.Size = new System.Drawing.Size(85, 17);
+            this.borchRadioButton.TabIndex = 0;
+            this.borchRadioButton.Tag = "45";
+            this.borchRadioButton.Text = "Borsch - 45₴";
+            this.borchRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // drinksGroupBox
+            // 
+            this.drinksGroupBox.Controls.Add(this.waterRadioButton);
+            this.drinksGroupBox.Controls.Add(this.latteRadioButton2);
+            this.drinksGroupBox.Controls.Add(this.coffeeRadioButton);
+            this.drinksGroupBox.Controls.Add(this.teaRadioButton);
+            this.drinksGroupBox.Controls.Add(this.juiceRadioButton);
+            this.drinksGroupBox.Location = new System.Drawing.Point(226, 22);
+            this.drinksGroupBox.Name = "drinksGroupBox";
+            this.drinksGroupBox.Size = new System.Drawing.Size(186, 146);
+            this.drinksGroupBox.TabIndex = 5;
+            this.drinksGroupBox.TabStop = false;
+            this.drinksGroupBox.Text = "Drinks";
+            // 
+            // waterRadioButton
+            // 
+            this.waterRadioButton.AutoSize = true;
+            this.waterRadioButton.Location = new System.Drawing.Point(6, 111);
+            this.waterRadioButton.Name = "waterRadioButton";
+            this.waterRadioButton.Size = new System.Drawing.Size(75, 17);
+            this.waterRadioButton.TabIndex = 4;
+            this.waterRadioButton.Tag = "5";
+            this.waterRadioButton.Text = "Water - 5₴";
+            this.waterRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // latteRadioButton2
+            // 
+            this.latteRadioButton2.AutoSize = true;
+            this.latteRadioButton2.Location = new System.Drawing.Point(6, 88);
+            this.latteRadioButton2.Name = "latteRadioButton2";
+            this.latteRadioButton2.Size = new System.Drawing.Size(76, 17);
+            this.latteRadioButton2.TabIndex = 3;
+            this.latteRadioButton2.Tag = "25";
+            this.latteRadioButton2.Text = "Latte - 25₴";
+            this.latteRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // coffeeRadioButton
+            // 
+            this.coffeeRadioButton.AutoSize = true;
+            this.coffeeRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.coffeeRadioButton.Name = "coffeeRadioButton";
+            this.coffeeRadioButton.Size = new System.Drawing.Size(86, 17);
+            this.coffeeRadioButton.TabIndex = 2;
+            this.coffeeRadioButton.Tag = "12";
+            this.coffeeRadioButton.Text = "Coffee - 12 ₴";
+            this.coffeeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // teaRadioButton
+            // 
+            this.teaRadioButton.AutoSize = true;
+            this.teaRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.teaRadioButton.Name = "teaRadioButton";
+            this.teaRadioButton.Size = new System.Drawing.Size(71, 17);
+            this.teaRadioButton.TabIndex = 1;
+            this.teaRadioButton.Tag = "12";
+            this.teaRadioButton.Text = "Tea - 12₴";
+            this.teaRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // juiceRadioButton
+            // 
+            this.juiceRadioButton.AutoSize = true;
+            this.juiceRadioButton.Location = new System.Drawing.Point(6, 19);
+            this.juiceRadioButton.Name = "juiceRadioButton";
+            this.juiceRadioButton.Size = new System.Drawing.Size(77, 17);
+            this.juiceRadioButton.TabIndex = 0;
+            this.juiceRadioButton.Tag = "10";
+            this.juiceRadioButton.Text = "Juice - 10₴";
+            this.juiceRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // dessertsGroupBox
+            // 
+            this.dessertsGroupBox.Controls.Add(this.iceCreamGadioButton6);
+            this.dessertsGroupBox.Controls.Add(this.chocoRadioButton);
+            this.dessertsGroupBox.Controls.Add(this.fruitsRadioButton);
+            this.dessertsGroupBox.Controls.Add(this.cakeRadioButton);
+            this.dessertsGroupBox.Controls.Add(this.puddingGadioButton10);
+            this.dessertsGroupBox.Location = new System.Drawing.Point(432, 22);
+            this.dessertsGroupBox.Name = "dessertsGroupBox";
+            this.dessertsGroupBox.Size = new System.Drawing.Size(186, 146);
+            this.dessertsGroupBox.TabIndex = 6;
+            this.dessertsGroupBox.TabStop = false;
+            this.dessertsGroupBox.Text = "Desserts";
+            // 
+            // iceCreamGadioButton6
+            // 
+            this.iceCreamGadioButton6.AutoSize = true;
+            this.iceCreamGadioButton6.Location = new System.Drawing.Point(6, 111);
+            this.iceCreamGadioButton6.Name = "iceCreamGadioButton6";
+            this.iceCreamGadioButton6.Size = new System.Drawing.Size(99, 17);
+            this.iceCreamGadioButton6.TabIndex = 4;
+            this.iceCreamGadioButton6.Tag = "25";
+            this.iceCreamGadioButton6.Text = "Ice cream - 25₴";
+            this.iceCreamGadioButton6.UseVisualStyleBackColor = true;
+            // 
+            // chocoRadioButton
+            // 
+            this.chocoRadioButton.AutoSize = true;
+            this.chocoRadioButton.Location = new System.Drawing.Point(6, 88);
+            this.chocoRadioButton.Name = "chocoRadioButton";
+            this.chocoRadioButton.Size = new System.Drawing.Size(100, 17);
+            this.chocoRadioButton.TabIndex = 3;
+            this.chocoRadioButton.Tag = "20";
+            this.chocoRadioButton.Text = "Chocolate - 20₴";
+            this.chocoRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // fruitsRadioButton
+            // 
+            this.fruitsRadioButton.AutoSize = true;
+            this.fruitsRadioButton.Location = new System.Drawing.Point(6, 65);
+            this.fruitsRadioButton.Name = "fruitsRadioButton";
+            this.fruitsRadioButton.Size = new System.Drawing.Size(80, 17);
+            this.fruitsRadioButton.TabIndex = 2;
+            this.fruitsRadioButton.Tag = "10";
+            this.fruitsRadioButton.Text = "Fruits - 10 ₴";
+            this.fruitsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // cakeRadioButton
+            // 
+            this.cakeRadioButton.AutoSize = true;
+            this.cakeRadioButton.Location = new System.Drawing.Point(6, 42);
+            this.cakeRadioButton.Name = "cakeRadioButton";
+            this.cakeRadioButton.Size = new System.Drawing.Size(112, 17);
+            this.cakeRadioButton.TabIndex = 1;
+            this.cakeRadioButton.Tag = "35";
+            this.cakeRadioButton.Text = "Cheesecake - 35₴";
+            this.cakeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // puddingGadioButton10
+            // 
+            this.puddingGadioButton10.AutoSize = true;
+            this.puddingGadioButton10.Location = new System.Drawing.Point(6, 19);
+            this.puddingGadioButton10.Name = "puddingGadioButton10";
+            this.puddingGadioButton10.Size = new System.Drawing.Size(91, 17);
+            this.puddingGadioButton10.TabIndex = 0;
+            this.puddingGadioButton10.Tag = "25";
+            this.puddingGadioButton10.Text = "Pudding - 25₴";
+            this.puddingGadioButton10.UseVisualStyleBackColor = true;
+            // 
+            // orderButton
+            // 
+            this.orderButton.Enabled = false;
+            this.orderButton.Location = new System.Drawing.Point(226, 232);
+            this.orderButton.Name = "orderButton";
+            this.orderButton.Size = new System.Drawing.Size(186, 73);
+            this.orderButton.TabIndex = 7;
+            this.orderButton.Text = "Make an order";
+            this.orderButton.UseVisualStyleBackColor = true;
+            this.orderButton.Click += new System.EventHandler(this.orderButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(679, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(210, 283);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 348);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.orderButton);
+            this.Controls.Add(this.dessertsGroupBox);
+            this.Controls.Add(this.drinksGroupBox);
+            this.Controls.Add(this.mainDishGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.mainDishGroupBox.ResumeLayout(false);
+            this.mainDishGroupBox.PerformLayout();
+            this.drinksGroupBox.ResumeLayout(false);
+            this.drinksGroupBox.PerformLayout();
+            this.dessertsGroupBox.ResumeLayout(false);
+            this.dessertsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox mainDishGroupBox;
+        private System.Windows.Forms.RadioButton carpRadioButton;
+        private System.Windows.Forms.RadioButton meatRadioButton;
+        private System.Windows.Forms.RadioButton potatoesRadioButton2;
+        private System.Windows.Forms.RadioButton pilafRadioButton;
+        private System.Windows.Forms.RadioButton borchRadioButton;
+        private System.Windows.Forms.GroupBox drinksGroupBox;
+        private System.Windows.Forms.RadioButton waterRadioButton;
+        private System.Windows.Forms.RadioButton latteRadioButton2;
+        private System.Windows.Forms.RadioButton coffeeRadioButton;
+        private System.Windows.Forms.RadioButton teaRadioButton;
+        private System.Windows.Forms.RadioButton juiceRadioButton;
+        private System.Windows.Forms.GroupBox dessertsGroupBox;
+        private System.Windows.Forms.RadioButton iceCreamGadioButton6;
+        private System.Windows.Forms.RadioButton chocoRadioButton;
+        private System.Windows.Forms.RadioButton fruitsRadioButton;
+        private System.Windows.Forms.RadioButton cakeRadioButton;
+        private System.Windows.Forms.RadioButton puddingGadioButton10;
+        private System.Windows.Forms.Button orderButton;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2__Controls
+{
+    public partial class Form1 : Form
+    {
+        int countOrders = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AddEvent();
+        }
+
+        private void orderButton_Click(object sender, EventArgs e)
+        {   
+            string res = "Total price: ";           
+
+            decimal totalPrice = 0;
+            foreach (RadioButton rb in drinksGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                    totalPrice += Convert.ToDecimal(rb.Tag);
+            }
+            foreach (RadioButton rb in mainDishGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                    totalPrice += Convert.ToDecimal(rb.Tag);
+            }
+            foreach (RadioButton rb in dessertsGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                    totalPrice += Convert.ToDecimal(rb.Tag);
+            }            
+
+            res += totalPrice.ToString() + "₴";
+            
+
+            MessageBox.Show($"{res}", $"#{++countOrders} {DateTime.Now.ToShortTimeString()}", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            string order = $"#{countOrders} {totalPrice.ToString()}₴ - {DateTime.Now.ToShortTimeString()}{Environment.NewLine}";
+            textBox1.Text += order.ToString();
+
+        }
+
+        private bool IsSelect()
+        {
+            bool isSelectedDrink = false;
+            bool isSelectedDish = false;
+            bool isSelectedDessert = false;
+
+            foreach (RadioButton rb in drinksGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    isSelectedDrink = true;
+                    break;
+                }
+            }
+            foreach (RadioButton rb in mainDishGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    isSelectedDish = true;
+                    break;
+                }
+            }
+
+            foreach (RadioButton rb in dessertsGroupBox.Controls.OfType<RadioButton>())
+            {
+                if (rb.Checked)
+                {
+                    isSelectedDessert = true;
+                    break;
+                }
+            }
+
+            return isSelectedDish && isSelectedDrink && isSelectedDessert;
+        }
+
+        private void AddEvent()
+        {
+            foreach (RadioButton rb in dessertsGroupBox.Controls.OfType<RadioButton>())
+            {
+                rb.CheckedChanged += RadioButtonCheckedChanged;
+            }
+
+            foreach (RadioButton rb in mainDishGroupBox.Controls.OfType<RadioButton>())
+            {
+                rb.CheckedChanged += RadioButtonCheckedChanged;
+            }
+
+            foreach (RadioButton rb in drinksGroupBox.Controls.OfType<RadioButton>())
+            {
+                rb.CheckedChanged += RadioButtonCheckedChanged;
+            }
+        }
+
+        private void RadioButtonCheckedChanged(object sender, EventArgs e)
+        {
+            orderButton.Enabled = IsSelect();
+        }
+       
+    }
+}
